1c313f51e0b1e6aeb9764c25161bc9a0426f9932
FrancoisMoanaMichael <91324853+FrancoisMoanaMichael@users.noreply.github.com>
2022-02-26 18:29:41 +0100

    basic submite form done

--- document-your-life/src/App/App.js
+++ document-your-life/src/App/App.js
@@ -1,9 +1,12 @@
+import FormSignIn from '../FormSignIn/formSignIn';
 import './App.css';
 
 function App() {
+  // TODO sup FormSingIn
   return (
     <div className="App">
-    
+      
+      <FormSignIn/>
     </div>
   );
 }
