fb4550342f4da499be6aabbbaeede1f8734f72c4
FrancoisMoanaMichael <91324853+FrancoisMoanaMichael@users.noreply.github.com>
2022-02-28 19:04:33 +0100

    notify instaled some css done function notifyfunc

--- document-your-life/src/Components/FormSignIn/formSignIn.jsx
+++ document-your-life/src/Components/FormSignIn/formSignIn.jsx
@@ -5,28 +5,41 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './formSignIn.scss';
 
+// react-router-dom
+import { useNavigate } from "react-router-dom";
+
 // axios post request
 import SignupAxios from '../../RequestsAxios/SignUp';
+
 // package npm 
 import PasswordChecklist from "react-password-checklist"
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Notify from '../../utils/notifyFunc';
 
 const FormSignIn = () => {
+  let navigate = useNavigate();
+
     const [firstName, setFirstName] = useState('')
     const [lastName, setLastName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const [passwordConf, setPasswordConf] = useState('')
     const [checkbox, setCheckbox] = useState(false)
+    const [checkboxText, setCheckboxText] = useState(false)
+    const [showModal, setShowModal] = useState(true)
 
 
    // password conditions
     // const [passwordLength, setPasswordLength] = useState(false)
     const [isValid, setIsValid] = useState(false)
-    
+    // Singup notification
+
+
 
    /**
      * 
-     * @param {event} e 
+     * @param {event}
      * change checkbox value ( true or false ) with onChange on checkbox
      */
     const handleCheckbox = (e) => {
@@ -56,22 +69,39 @@ const FormSignIn = () => {
          && isValid === true
          ){
             SignupAxios(email,firstName,lastName,password,passwordConf)
+            setShowModal(false)
+            Notify(`Salut ${firstName}, Bravo pour ton inscription`,"success")
             console.log('submited')
+            navigate('/login', { replace: true })
             }
         else{
+            Notify(`Une erreur est survenue lors de l'inscription`,"error")
                 console.log('please check form')
+                setCheckboxText(true)
                 return null
             }
             console.log(isValid)
     }
 
-			
+
    return (
+       <>
+        <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        />{showModal?
        <div className='formSignin'>
             <h2 className='formSignin-title'>Inscription</h2>
                 <form className='formSignin-form' onSubmit={handleSubmit}>
 
-                <p className='formSignin-form-title'>Prénom</p>
+                <h3 className='formSignin-form-title'>Prénom</h3>
                 <input 
                 className='formSignin-form-input'
                 value={firstName}
@@ -79,7 +109,7 @@ const FormSignIn = () => {
                 type="text"
                 placeholder='Entrez votre prénom'/>
 
-                <p className='formSignin-form-title'>Nom</p>
+                <h3 className='formSignin-form-title'>Nom</h3>
                 <input
                 className='formSignin-form-input'
                 value={lastName}
@@ -87,7 +117,7 @@ const FormSignIn = () => {
                 type="text"
                 placeholder='Entrez votre nom'/>
 
-                <p className='formSignin-form-title'>Email</p>
+                <h3 className='formSignin-form-title'>Email</h3>
                 <input
                 className='formSignin-form-input'
                 value={email}
@@ -95,7 +125,7 @@ const FormSignIn = () => {
                 type="email"
                 placeholder='Entrez votre Email'/>
 
-                <p className='formSignin-form-title'>Mot de passe</p>
+                <h3 className='formSignin-form-title'>Mot de passe</h3>
                 <input
                 className='formSignin-form-input'
                 value={password}
@@ -104,17 +134,17 @@ const FormSignIn = () => {
                 placeholder='Entrez votre Mot de passe'/>
               
                 <PasswordChecklist
-				rules={["minLength","specialChar","number","capital","match"]}
+                className='formSignin-form-checklist'
+				rules={["minLength","specialChar","number","capital"]}
 				minLength={8}
 				value={password}
 				valueAgain={passwordConf}
 				onChange={handleIsValid}
 				messages={{
-					minLength: "8 Caractère minimume.",
-					specialChar: "1 Caractère spécial minimume.",
-					number: "1 Chiffre minimume.",
-					capital: "1 Majuscule minimume.",
-					match: "Mot de passe différent.",
+					minLength: "8 caractères minimum.",
+					specialChar: "1 caractère spécial minimum.",
+					number: "1 chiffre minimum.",
+					capital: "1 majuscule minimum.",
 				}}/>
  
                 
@@ -124,15 +154,29 @@ const FormSignIn = () => {
                 onChange={(e)=>setPasswordConf(e.target.value)}
                 type="password"
                 placeholder='Confirmez votre Mot de passe'/>
+                
+                <PasswordChecklist
+                className='formSignin-form-checklist'
+				rules={["match"]}
+				minLength={8}
+				value={password}
+				valueAgain={passwordConf}
+				onChange={handleIsValid}
+				messages={{
+					match: "Mots de passe identiques.",
+				}}/>
                 <div className='formSignin-form-checkbox'>
                 <input className='formSignine-form-checkbox-input' type="checkbox" name='checkbox' onChange={handleCheckbox}/>
-                <label> J'accepte les conditions d'utilisation </label>
+                <p> J'accepte les conditions d'utilisation </p>
+                {checkboxText? <p className='formSignin-form-checkbox-error'>Acceptez les conditions d'utilisation </p>: null}
                 </div>
           <button type="submit" onSubmit={handleSubmit} className="formSignin-form-btn">
             Envoyer
           </button>
                 </form>
-       </div>
+       </div> 
+        : null
+                }</>
    );
 };
 
