8e9d39165288a63b73cd1d12b6885686bba64cd6
Jérôme Richard <jerrylejer23@yahoo.fr>
2022-03-01 13:20:53 +0100

    Card component + scss

--- document-your-life/src/App/App.js
+++ document-your-life/src/App/App.js
@@ -1,8 +1,9 @@
 /* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable react/jsx-key */
 import HeaderNavbar from '../Components/HeaderNavbar/HeaderNavbar';
-import TabDashboard from '../Components/TabDashboard/TabDashboard';
-import Calendar from '../Components/Calendar/Calendar';
+// import TabDashboard from '../Components/TabDashboard/TabDashboard';
+// import Calendar from '../Components/Calendar/Calendar';
+import Card from '../Components/Card/Card';
 import './App.css';
 
 function App() {
@@ -10,8 +11,9 @@ function App() {
   return (
     <div className="App">
       <HeaderNavbar/>
-      <TabDashboard/>
-      <Calendar /> 
+      {/* <TabDashboard/> */}
+      {/* <Calendar />  */}
+      <Card />
     </div>
   );
 }
