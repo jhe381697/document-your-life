967b771ea6bc1d3835649669dc58d4d1b2ab6937
Josephine Herlemont <herlemont.josephine@gmail.com>
2022-03-02 15:37:58 +0100

    CardEdit component sans scss

--- document-your-life/src/App/App.js
+++ document-your-life/src/App/App.js
@@ -10,6 +10,7 @@ import { Routes, Route } from "react-router-dom";
 import HeaderNavbar from '../Components/HeaderNavbar/HeaderNavbar';
 import TabDashboard from '../Components/TabDashboard/TabDashboard';
 import FormSignIn from "../Components/FormSignIn/formSignIn";
+import Card from "../Components/Card/Card";
 
 import HomePage from "../Components/HomePage/HomePage";
 import Login from "../Components/LoginForm/LoginForm";
@@ -34,10 +35,11 @@ function App() {
           <Login />}
         />
         <Route path="/dashboard/*" element={
-          <TabDashboard />
-        }
+          <TabDashboard />}
         />
-
+        <Route path="/card" element={
+           <Card />}
+        />
       </Routes>
     </div>
   );
