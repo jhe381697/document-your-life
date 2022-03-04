e1ec4ada9d27aadfd5537286caece9c4ef3cb7d1
Josephine Herlemont <herlemont.josephine@gmail.com>
2022-03-04 15:50:16 +0100

    CardResume scss

--- document-your-life/src/Components/CardResume/CardResume.js
+++ document-your-life/src/Components/CardResume/CardResume.js
@@ -1,9 +1,22 @@
+/* eslint-disable react/jsx-key */
 import React from 'react'
 import './cardResume.scss'
 
 const CardResume = () => {
   return (
-    <div>CardResume</div>
+    <div className='cardresume-container'>
+      <div className='cardresume'>
+        <h2>Date de la journée</h2>
+        <div className='cardresume-mood'>
+          <h3>Humeur de la journée</h3>
+          <p>emoji</p>
+        </div>
+        <div className='cardresume-medium'>
+          <h3>Résumé de la journée</h3>
+          <p>medium</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
