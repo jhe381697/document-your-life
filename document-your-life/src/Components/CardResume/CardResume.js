c1ad73a6336fe6087cee381d0978bdd56459fa06
Josephine Herlemont <herlemont.josephine@gmail.com>
2022-03-07 11:16:10 +0100

    wip CardResume

--- document-your-life/src/Components/CardResume/CardResume.js
+++ document-your-life/src/Components/CardResume/CardResume.js
@@ -1,20 +1,27 @@
 /* eslint-disable react/jsx-key */
 import React from 'react'
+import { useState, useEffect } from 'react';
 import './cardResume.scss'
 
 const CardResume = () => {
 
+  const [value, setValue] = useState();
+
+  useEffect(() => {
+    setValue(1);  // 1 remplacé par la réponse axios
+  }, [])
+  
   return (
     <div className='cardresume-container'>
       <div className='cardresume'>
         <h2>Date de la journée</h2>
         <div className='cardresume-mood'>
           <h3>Humeur de la journée</h3>
-          <p>emoji</p>
+          <div className='cardresume-mood-emoji'>{value}</div>
         </div>
         <div className='cardresume-medium'>
           <h3>Résumé de la journée</h3>
-          <p>medium</p>
+          <div className='cardresume-medium-infos'>medium</div>
         </div>
       </div>
     </div>
