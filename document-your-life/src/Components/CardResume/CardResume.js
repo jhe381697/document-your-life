7edece5d6e9b9fb2b709dfd517ecce86cd31e42a
Josephine Herlemont <herlemont.josephine@gmail.com>
2022-03-08 13:20:05 +0100

    wip

--- document-your-life/src/Components/CardResume/CardResume.js
+++ document-your-life/src/Components/CardResume/CardResume.js
@@ -1,22 +1,39 @@
 /* eslint-disable react/jsx-key */
 import React from 'react'
 import { useState, useEffect } from 'react';
+import { getTodayCard } from '../../RequestsAxios/CardsReq';
 import './cardResume.scss'
 
 const CardResume = () => {
 
   // const [value, setValue] = useState();
-  const [mood, setMood] = useState();
-  const [medium, setMedium] = useState();
+  const [mood, setMood] = useState([]);
+  // const [mediums, setMediums] = useState([]);
+
+  const todayCardData = async () => {
+    const todayCard = await getTodayCard();
+    console.log(todayCard);
+    if (todayCard.status === 200)
+      {
+        setMood(todayCard.data.lastCards[0].moodlabel);
+        // setMediums(todayCard.data.lastCards[0].audio);
+        // setMediums(todayCard.data.lastCards[0].image);
+        // setMediums(todayCard.data.lastCards[0].text);
+        // setMediums(todayCard.data.lastCards[0].video);
+        // const cardDate = todayCard.data.lastCards[0].created_at;
+        return
+      }
+    else {
+      todayCardData();
+      console.log('erreur')
+    }
+  };
 
   useEffect(() => {
-    setMood("emoticone");  // "emoticone" remplacé par la réponse axios
+    todayCardData();
   }, [])
+  console.log(mood);
 
-  useEffect(() => {
-    setMedium("medium");
-  }, [])
-  
   return (
     <div className='cardresume-container'>
       <div className='cardresume'>
@@ -27,7 +44,11 @@ const CardResume = () => {
         </div>
         <div className='cardresume-medium'>
           <h3>Résumé de la journée</h3>
-          <div className='cardresume-medium-infos'>{medium}</div>
+          <div className='cardresume-medium-infos'>
+            {/* {mediums.map((medium) => (
+              {medium}
+            ))} */}
+          </div>
         </div>
       </div>
     </div>
