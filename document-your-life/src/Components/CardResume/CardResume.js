54cecce304be7d1ffea1cd8e28c5bc26687fb7f7
Josephine Herlemont <herlemont.josephine@gmail.com>
2022-03-09 10:47:44 +0100

    wip

--- document-your-life/src/Components/CardResume/CardResume.js
+++ document-your-life/src/Components/CardResume/CardResume.js
@@ -2,8 +2,10 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
 import { getTodayCard } from '../../RequestsAxios/CardsReq';
-import './cardResume.scss'
 import { Link } from "react-router-dom"
+
+import './cardResume.scss'
+
 const CardResume = () => {
   const [date, setDate] = useState();
   const [mood, setMood] = useState([]);
@@ -40,39 +42,39 @@ const CardResume = () => {
 
   return (
     <Link to='/card'>
-    <div className='cardresume-container'>
-      <div className='cardresume'>
-        <h2>{date}</h2>
-        <div className='cardresume-mood'>
-          <h3>Humeur de la journée</h3>
-          <div className='cardresume-mood-emoji'>{mood}</div>
-        </div>
-        <div className='cardresume-medium'>
-          <h3>Résumé de la journée</h3>
-          <div className='cardresume-medium-infos'>
-            {texts ? 
-            texts.map((text) => (
-              <div>{text}</div>
-            )) : null}
+      <div className='cardresume-container'>
+        <div className='cardresume'>
+          <h2>{date}</h2>
+          <div className='cardresume-mood'>
+            <h3>Humeur de la journée</h3>
+            <div className='cardresume-mood-emoji'>{mood}</div>
+          </div>
+          <div className='cardresume-medium'>
+            <h3>Résumé de la journée</h3>
+            <div className='cardresume-medium-infos'>
+              {texts ? 
+              texts.map((text) => (
+                <div>{text}</div>
+              )) : null}
 
-            {sounds ?
-            sounds.map((sound) => (
-              <div>{sound}</div>
-            )) : null}
+              {sounds ?
+              sounds.map((sound) => (
+                <div>{sound}</div>
+              )) : null}
 
-            {pictures ?
-            pictures.map((picture) => (
-              <div>{picture}</div>
-            )) : null}
+              {pictures ?
+              pictures.map((picture) => (
+                <div>{picture}</div>
+              )) : null}
 
-            {videos ?
-            videos.map((video) => (
-              <div>{video}</div>
-            )) : null}
+              {videos ?
+              videos.map((video) => (
+                <div>{video}</div>
+              )) : null}
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </Link>
   )
 }
